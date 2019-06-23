5c09cd181c1781e5bf1af334899e0630e7ef2d6a
Diego Arias <d.arias2000.eh@gmail.com>
2019-06-23 01:03:09 -0600

    Siuuu

---
+++ views/dashboard/main.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../../core/helpers/dashboard.php');
+Dashboard::headerTemplate('¡Ingresastes al sitio privado de RealTech!');
+?>
+<div class="container">
+    <div class="row">
+	    <!-- <h4 class="center-align blue-text" id="greeting"></h4> -->
+    </div>
+</div>
+<?php
+Dashboard::footerTemplate('main.js');
+?>
